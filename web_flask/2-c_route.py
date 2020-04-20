e2e58a5db6a116711e81655f3b4e4f0a5fe3ff82
Ben Keener <keener4christ@gmail.com>
2020-04-20 18:21:32 +0000

    Completed task 2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,26 @@
+"""This module starts a simple flask server"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_world():
+    """Sends simple text"""
+    return ("Hello HBNB!\n")
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hello_hbnb():
+    """Sends simple text"""
+    return ("HBNB\n")
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def dyn_text(text):
+    """Returns dynamic text"""
+    return ("C {}\n".format(text.replace("_", " ")))
+
+if __name__ == "__main__":
+    app.run()
