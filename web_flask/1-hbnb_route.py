7b02e7643c8856edf79ea3dbc8153672308c8641
Ben Keener <keener4christ@gmail.com>
2020-04-22 01:19:23 +0000

    pep8'd

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -11,10 +11,12 @@ def hello_world():
     """Sends simple text"""
     return ("Hello HBNB!")
 
+
 @app.route("/hbnb", strict_slashes=False)
 def hello_hbnb():
     """Sends simple text"""
     return ("HBNB")
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
